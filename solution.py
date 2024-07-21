f199dae54ea9450a381c5dd84f41d0843f8b8ab3
Kooreis <kooreis.official@gmail.com>
2024-07-21 00:43:48 -0500

    This is the function that calculates the total number of set bits in all numbers from 1 to n. It uses bitwise operations to count the set bits.

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
-Here is a Python console application that calculates the total number of set bits in all numbers from 1 to N.
-
-```python
 def count_set_bits(n):
     n += 1
     count = 0
@@ -10,10 +7,3 @@ def count_set_bits(n):
         count += div * (1 << bit)
         count += min(max(rem - (1 << bit), 0), (1 << bit))
     return count
-
-if __name__ == "__main__":
-    n = int(input("Enter a number: "))
-    print("Total set bits: ", count_set_bits(n))
-```
-
-You can run this console application, it will ask you to enter a number and then it will print the total number of set bits in all numbers from 1 to the entered number.
