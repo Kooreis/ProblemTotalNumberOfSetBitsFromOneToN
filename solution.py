8d2861ee43b68b815ddca3b1487280367d3d3a0c
Kooreis <kooreis.official@gmail.com>
2024-07-22 00:43:48 -0500

    This part of the script prompts the user to enter a number when the script is run directly.

--- solution.py
+++ solution.py
@@ -1,9 +1,2 @@
-def count_set_bits(n):
-    n += 1
-    count = 0
-    for bit in range(32):
-        div = n // (1 << (bit + 1))
-        rem = n % (1 << (bit + 1))
-        count += div * (1 << bit)
-        count += min(max(rem - (1 << bit), 0), (1 << bit))
-    return count
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
